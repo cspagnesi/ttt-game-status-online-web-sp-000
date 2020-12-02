ca6986f123b9b02501497a3a6eae9a21474b5498
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-12-02 15:17:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,6 +15,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
+  
   WIN_COMBINATIONS.each do |single_win_combo|
     win_index_1 = single_win_combo[0]
     win_index_2 = single_win_combo[1]
@@ -26,5 +27,6 @@ def won?(board)
    
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return single_win_combo
+      
     end
   end
