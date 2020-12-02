cf059d5fb31ce0880a862c1c9d59690344f13f98
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-12-02 15:18:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,61 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+def display_board(board)
+    
+puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
+puts ("-----------")
+puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
+puts ("-----------")
+puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
+      
+    end
+    
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, marker)
+ board[index] = marker
+end
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
+  end
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
 end
 
 WIN_COMBINATIONS = [
@@ -15,7 +70,6 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  
   WIN_COMBINATIONS.each do |single_win_combo|
     win_index_1 = single_win_combo[0]
     win_index_2 = single_win_combo[1]
@@ -28,5 +82,58 @@ def won?(board)
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return single_win_combo
     end
-    end
   end
+  return false
+end
+
+def full?(board)
+  if board.any? {|index| index == nil || index == " "}
+    return false
+  else
+    return true
+  end
+end
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif!full?(board) && !won?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board) 
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
+
+def play(board)
+  counter = 0
+  until counter == 9
+  turn(board)
+  counter += 1
+  end
+end
+
+def play(board)
+  until over?(board)
+    turn(board) 
+  end
+  if won?(board)
+    winner(board) == "X" || winner(board) == "O"
+    puts "Congratulations #{winner(board)}!"
+  else draw?(board)
+    puts "Cat\'s Game!"
+  end
+end
